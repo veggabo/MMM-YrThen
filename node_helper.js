f319e7f461dd39ba0891a4f2b6b7baa9264654cb
Vegard Bøe <vgrdboee@gmail.com>
2025-02-05 21:38:09 +0100

    Update node_helper.js
    updated node_helper.js after running into errors when using the new code in MMM-YrThen.js.

--- node_helper.js
+++ node_helper.js
@@ -1,59 +1,69 @@
-// Reminder to self: tail -f ~/.pm2/logs/MagicMirror-out-0.log
-var NodeHelper = require('node_helper');
-var request = require('request');
+const NodeHelper = require('node_helper');
+const request = require('request');
 
-var d = new Date();
-var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-// console.log("MMM-YrThen " + n + ": Starting MMM-YrThen in node_helper");
+const d = new Date();
+const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+// console.log(`MMM-YrThen ${n}: Starting MMM-YrThen in node_helper`);
 
 module.exports = NodeHelper.create({
     start: function() {
-        var d = new Date();
-        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-        console.log("MMM-YrThen " + n + ": Starting helper for MMM-YrThen");
+        const d = new Date();
+        const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+        console.log(`MMM-YrThen ${n}: Starting helper for MMM-YrThen`);
         this.config = null;
         this.forecastUrl = '';
     },
 
     socketNotificationReceived: function(notification, payload) {
-        var self = this;
-        var d = new Date();
-        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//        console.log("MMM-YrThen " + n + ": Received notification");
-        if(notification === 'GET_YRTHEN_FORECAST') {
-            var d = new Date();
-            var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//            console.log("MMM-YrThen " + n + ": Received GET_YRTHEN_FORECAST notification");
+        const self = this;
+        const d = new Date();
+        const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+//        console.log(`MMM-YrThen ${n}: Received notification`);
+        if (notification === 'GET_YRTHEN_FORECAST') {
+            const d = new Date();
+            const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+//            console.log(`MMM-YrThen ${n}: Received GET_YRTHEN_FORECAST notification`);
             self.config = payload.config;
             self.forecastUrl = payload.forecastUrl;
             this.getForecastFromYrThen();
         }
     },
 
-    getForecastFromYrThen: function() {
-        var d = new Date();
-        var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//        console.log("MMM-YrThen " + n + ": Getting forecast");
-        var self = this;
-        var locationData = {};
+    getForecastFromYrThen: async function() {
+        const d = new Date();
+        const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+//        console.log(`MMM-YrThen ${n}: Getting forecast`);
+        const self = this;
+        const locationData = {};
 
-        request({url: self.forecastUrl, method: 'GET'}, function(error, response, message) {
-            var d = new Date();
-            var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//            console.log("MMM-YrThen " + n + ": Requesting forecast from" + self.forecastUrl);
-            if (!error && (response.statusCode == 200 || response.statusCode == 304)) {
-                locationData.forecast = JSON.parse(message);
-                var d = new Date();
-                var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//                console.log("MMM-YrThen " + n + ": Returning forecast");
+        try {
+            const response = await this.makeRequest(self.forecastUrl);
+            if (response.statusCode === 200 || response.statusCode === 304) {
+                locationData.forecast = JSON.parse(response.body);
+                const d = new Date();
+                const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+//                console.log(`MMM-YrThen ${n}: Returning forecast`);
                 self.sendSocketNotification('YRTHEN_FORECAST_DATA', locationData);
+            } else {
+                const d = new Date();
+                const n = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+//                console.log(`MMM-YrThen ${n}: Error!`);
+                throw new Error(`Error fetching forecast: ${response.statusCode}`);
             }
-            else{
-                var d = new Date();
-                var n = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-//                console.log("MMM-YrThen " + n + ": Error!");
-            }
+        } catch (error) {
+            console.error('Error fetching forecast:', error);
+        }
+    },
+
+    makeRequest: function(url) {
+        return new Promise((resolve, reject) => {
+            request(url, (error, response, body) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve({ statusCode: response.statusCode, body: body });
+                }
+            });
         });
     }
-
 });
